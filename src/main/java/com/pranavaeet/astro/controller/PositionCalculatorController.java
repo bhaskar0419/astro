525fb7142b900678d5fba0d157a86f5a6fe38d0a
bhaskar0419 <108991290+bhaskar0419@users.noreply.github.com>
2024-07-07 17:44:17 +0530

    Update PositionCalculatorController.java

--- src/main/java/com/pranavaeet/astro/controller/PositionCalculatorController.java
+++ src/main/java/com/pranavaeet/astro/controller/PositionCalculatorController.java
@@ -1,19 +1,21 @@
 package com.pranavaeet.astro.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.pranavaeet.astro.entity.InputParameters;
 import com.pranavaeet.astro.service.CalculationService;
 import com.pranavaeet.astro.service.OutputStructure;
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 
 @Controller
@@ -29,13 +31,13 @@ public class PositionCalculatorController {
     }
 
     @PostMapping("calculate")
-    public String positionCalculation(@RequestParam("lat") double lat, @RequestParam("lon") double lon, @RequestParam("datetime") String datetime, Model model){
+    public String positionCalculation(@ModelAttribute InputParameters inputParameters, Model model){
 
-        List<OutputStructure> planetslocations=calculationService.LocationCalculator(lat,lon,datetime);
+        List<OutputStructure> planetslocations=calculationService.LocationCalculator(inputParameters.getLat(),inputParameters.getLon(),inputParameters.getDatetime());
         model.addAttribute("planetslocations",planetslocations);
-        System.out.println("lat"+lat);
-        System.out.println("lon"+lon);
-        System.out.println("datetime"+datetime);
+        System.out.println("lat"+inputParameters.getLat());
+        System.out.println("lon"+inputParameters.getLon());
+        System.out.println("datetime"+inputParameters.getDatetime());
         for(OutputStructure outputStructure:planetslocations){
             System.out.println(outputStructure.planetname);
             System.out.println(outputStructure.r);
@@ -45,4 +47,26 @@ public class PositionCalculatorController {
         }
         return "planetpositions";
     }
+
+    @PostMapping("calculate2")
+    public ResponseEntity<Object> positionCalculation2(@RequestBody InputParameters inputParameters){
+
+        List<OutputStructure> planetslocations=calculationService.LocationCalculator(inputParameters.getLat(),inputParameters.getLon(),inputParameters.getDatetime());
+        System.out.println("lat"+inputParameters.getLat());
+        System.out.println("lon"+inputParameters.getLon());
+        System.out.println("datetime"+inputParameters.getDatetime());
+        // for(OutputStructure outputStructure:planetslocations){
+        //     System.out.println(outputStructure.planetname);
+        //     System.out.println(outputStructure.r);
+        //     System.out.println(outputStructure.phi);
+        //     System.out.println(outputStructure.elevation);
+        //     System.out.println(outputStructure.azimuth);
+        // }
+        return new ResponseEntity<>(planetslocations,HttpStatus.OK);
+    }
+
 }
+
+
+
+
